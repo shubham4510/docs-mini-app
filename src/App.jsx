0a4aaf3416313974d8a8d362061d8687a7006511
Shubham <shubhambhardwaj112277@gmail.com>
2024-05-17 22:56:09 +0530

    Initialize Docs Mini App

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Background from './components/Background'
+
+const App = () => {
+  return (
+    <div className=' h-screen w-full relative bg-zinc-900 '>
+
+    <Background/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Background.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Background = () => {
+  return (
+ <div className=''>
+    <div className=' text-zinc-400 py-10 text-center font-semibold'>  
+    DOCS
+  </div>
+  <h1 className=' text-[13rem] text-zinc-600 tracking-tighter absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] font-semibold'>DOCS.</h1></div>
+  )
+}
+
+export default Background
